52d136d6d498cbd673b961f0ec040c6da2573869
ahmed-kamar-elabs <ahmed.shaban.kamar@gmail.com>
2021-08-01 22:39:56 +0200

    add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginOrRegisterComponent } from './pages/outer-pages/authentication/login-or-register/login-or-register.component';
+import { HeaderComponent } from './theming/header/header.component';
+import { FooterComponent } from './theming/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginOrRegisterComponent
+    LoginOrRegisterComponent,
+    HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
